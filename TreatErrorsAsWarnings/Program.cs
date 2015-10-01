29597b602d7e5f5509a94b52fbba12b5d7f269c6
Joe Rall <Joseph.Rall@Live.com>
2015-10-01 00:21:54 -0400

    Initial Comit

---
+++ TreatErrorsAsWarnings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreatErrorsAsWarnings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("X is not used");
+        }
+    }
+}
